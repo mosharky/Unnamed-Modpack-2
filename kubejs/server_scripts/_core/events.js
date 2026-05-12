8fe63334b05242a42e6c8ad5fe6fe85068b79a71
mo_shark <medoelzarkalt@gmail.com>
2026-05-12 05:00:31 -0400

    minor entity disable fix

--- kubejs/server_scripts/_core/events.js
+++ kubejs/server_scripts/_core/events.js
@@ -1,4 +1,4 @@
-BlockEvents.rightClicked('minecraft:end_portal_frame', e => antiLoser(e, 'minecraft:ender_eye', 'alexsmobs:bear_dust', 5))
-BlockEvents.rightClicked('minecraft:soul_sand', e => antiLoser(e, 'minecraft:wither_skeleton_skull', 'alexsmobs:bear_dust', 5))
-
-EntityEvents.checkSpawn(global.ENTITY_REMOVALS, e => e.success(false))
+EntityEvents.checkSpawn(global.ENTITY_REMOVALS, e => {
+    console.log(`Preventing spawn of ${e.entity.type} at ${e.entity.x}, ${e.entity.y}, ${e.entity.z}`)
+    e.success(false)
+})
